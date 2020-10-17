f07e859e4012357acb88523bb47a7421d8f2bf8f
asgerb <a@asgerbehnckejacobsen.dk>
2020-10-17 15:22:13 +0200

    Add view_components.rb

---
+++ app/components/vime/view_components.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "view_component/engine"
+
+require_relative "component"
+
+require_relative "caption_control"
+require_relative "control_group"
+require_relative "controls"
+require_relative "default_ui"
+require_relative "fullscreen_control"
+require_relative "mute_control"
+require_relative "playback_control"
+require_relative "player"
+require_relative "scrim"
+require_relative "scrubber_control"
+require_relative "settings_control"
+require_relative "video"
+require_relative "vimeo"
+require_relative "volume_control"
